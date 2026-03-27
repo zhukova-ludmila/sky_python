b3a4961204642b69b8cc088d8cf55633b5684dbc
zhukova-ludmila <zhukova-ludmila-2001@yandex.ru>
2026-03-27 13:34:43 +0300

    commit11

---
+++ lesson_1/lesson_1_training.py
@@ -0,0 +1,21 @@
+my_heigh = 164
+print(my_heigh)
+
+my_name = "Людмила"
+my_name = "Людмила Жукоа"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
